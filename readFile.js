d30b4039be94a8152a738a08a20b0579c097ecb9
Weiwei-png <66428139+Weiwei-png@users.noreply.github.com>
2020-06-22 23:54:49 +0100

    Update readFile.js

--- readFile.js
+++ readFile.js
@@ -1,15 +1,7 @@
 let imgUrl=[
-    'demo2.jpg',
+    '1.jpg',
     '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '13.jpg',
-    '14.jpg',
-    '15.jpg',
-    '16.jpg',
+   
     
 ]
 window.g={
